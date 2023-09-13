f39db86fb79bcdf6b0e508c70c07aa7183561296
Louie Shin <unicus.fortis@gmail.com>
2023-09-14 03:08:41 +0900

    Initial Commit

---
+++ LotteryAPIwithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LotteryAPIwithMVVM
+//
+//  Created by LOUIE MAC on 2023/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
